ed3f2db5d3a6d7c5e829fb79901bbcd132deb876
José Sousa <126251504+sprugit@users.noreply.github.com>
2025-04-15 18:42:07 +0100

    Deployment feature 1.2

--- build-image.bat
+++ build-image.bat
@@ -1,4 +1,6 @@
 @ECHO OFF
+SET previous=%cd%
 CD %~dp0
 CALL mvnw.cmd clean package -Pnative -DskipTests --define quarkus.native.container-build=true --define quarkus.container-image.build=true
 docker build -f src\main\docker\Dockerfile.jvm -t tripmonkey/workspace-service .
+CD %previous%
