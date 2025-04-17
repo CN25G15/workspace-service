e0f1a4473c39f0f423c421abdb37d92ef985126e
José Sousa <126251504+sprugit@users.noreply.github.com>
2025-04-17 01:27:15 +0100

    Deployment feature 2.0.0 - splitting up kubernetes uber yaml

--- build-image.bat
+++ build-image.bat
@@ -2,5 +2,5 @@
 SET previous=%cd%
 CD %~dp0
 CALL mvnw.cmd clean package -Pnative -DskipTests --define quarkus.native.container-build=true --define quarkus.container-image.build=true
-docker build -f src\main\docker\Dockerfile.jvm -t tripmonkey/workspace-service .
+docker build -f src\main\docker\Dockerfile.jvm -t sprugit/workspace-service .
 CD %previous%
