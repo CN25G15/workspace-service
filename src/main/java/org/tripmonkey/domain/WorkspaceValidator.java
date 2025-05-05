3b6bfcc0a49036801ede89c6abb5fa052448fe81
José Sousa <126251504+sprugit@users.noreply.github.com>
2025-05-05 21:17:09 +0100

    GooglePlaces Implementation + Kafka Registry Removal

--- src/main/java/org/tripmonkey/domain/WorkspaceValidator.java
+++ src/main/java/org/tripmonkey/domain/WorkspaceValidator.java
@@ -1,15 +1,29 @@
 package org.tripmonkey.domain;
 
+import io.quarkus.grpc.GrpcClient;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.logging.Logger;
+import org.tripmonkey.database.service.PersistUserData;
+import org.tripmonkey.database.service.UserPlace;
+import org.tripmonkey.domain.data.Location;
 import org.tripmonkey.google.places.PlacesClientInterface;
+import org.tripmonkey.google.places.data.APIResponse;
+import org.tripmonkey.proto.domain.ProtoMapper;
+import org.tripmonkey.proto.google.places.data.Place;
+import org.tripmonkey.rest.domain.WorkspacePatch;
+import org.tripmonkey.workspace.service.User;
 
+import java.util.List;
 import java.util.Optional;
 
 @ApplicationScoped
 public class WorkspaceValidator {
+
+    @Inject
+    Logger log;
 
     @ConfigProperty(name = "api.key.google")
     Optional<String> key;
@@ -17,5 +31,48 @@ public class WorkspaceValidator {
     @Inject
     @RestClient
     PlacesClientInterface placesClient;
+
+    @GrpcClient("psud")
+    PersistUserData psud;
+
+    public boolean validate(WorkspacePatch wp) {
+
+        if(wp.getValue().getValue() instanceof Location l) {
+
+            if(key.isPresent()){
+                APIResponse r = placesClient.getLocationDetails(key.get(), List.of("place_id","name","type","geometry","rating"), l.getPlaceId());
+                switch(r.getStatus()){
+                    case REQUEST_DENIED -> {
+                        log.warnf("API key provided for google endpoints is invalid.\nAccepting location as valid even though it may not be valid.");
+                        return true;
+                    }
+                    case INVALID_REQUEST -> {
+                        return false;
+                    }
+                    case OK -> {
+                        psud.store(UserPlace.newBuilder()
+                                .setPlace(Place.newBuilder().setPlaceId(r.getResult().getPlace_id())
+                                        .setRating(r.getResult().getRating())
+                                        .addAllType(r.getResult().getTypes())
+                                        .setGeometry(ProtoMapper.geometryMapper.serialize(r.getResult().getGeometry()))
+                                        .build())
+                                .setUser(User.newBuilder().setUserId(wp.getUser()).build())
+                                .build()).map(status -> {
+                                    if(status.getStatus() != 200){
+                                        log.errorf("Couldn't store location with user in database.");
+                                    }
+                                    return status;
+                        });
+                        return true;
+                    }
+                }
+            } else {
+              log.warnf("No API key for google endpoints.\nAccepting location as valid even though it may not be valid.");
+            }
+
+        }
+        return true;
+
+    }
 
 }
--- src/main/java/org/tripmonkey/service/WorkspaceCreatorService.java
+++ src/main/java/org/tripmonkey/service/WorkspaceCreatorService.java
@@ -3,9 +3,11 @@ package org.tripmonkey.service;
 import io.quarkus.grpc.GrpcClient;
 import io.quarkus.grpc.GrpcService;
 import io.smallrye.mutiny.Uni;
+import jakarta.inject.Inject;
+import org.jboss.logging.Logger;
 import org.tripmonkey.database.service.CreateWorkspace;
 import org.tripmonkey.domain.data.Workspace;
-import org.tripmonkey.proto.ProtoSerde;
+import org.tripmonkey.proto.domain.ProtoMapper;
 import org.tripmonkey.workspace.service.User;
 import org.tripmonkey.workspace.service.WorkspaceCreator;
 import org.tripmonkey.workspace.service.WorkspaceResponse;
@@ -13,13 +15,18 @@ import org.tripmonkey.workspace.service.WorkspaceResponse;
 @GrpcService
 public class WorkspaceCreatorService implements WorkspaceCreator {
 
+    @Inject
+    Logger log;
+
     @GrpcClient("cwk")
     CreateWorkspace cwk;
 
     @Override
     public Uni<WorkspaceResponse> create(User request) {
-        return Uni.createFrom().item(ProtoSerde.userMapper.deserialize(request))
+        log.infof("Received request to create! %s", request.getUserId());
+        return Uni.createFrom().item(ProtoMapper.userMapper.deserialize(request))
+                .log(String.format("Received request to create workspace for user %s", request.getUserId()))
                 .map(Workspace::createFor)
-                .map(ProtoSerde.workspaceMapper::serialize).onItem().transformToUni(cwk::create);
+                .map(ProtoMapper.workspaceMapper::serialize).onItem().transformToUni(cwk::create);
     }
 }
